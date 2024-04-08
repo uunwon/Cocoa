31c0e3e6f8a0b4be654f7f71c1e604372336ce1d
uunwon <chea2089@naver.com>
2024-04-08 17:36:03 +0900

    Picker - Segment

--- RedSwiftUI/RedSwiftUI/PickerView.swift
+++ RedSwiftUI/RedSwiftUI/PickerView.swift
@@ -30,6 +30,14 @@ struct PickerView: View {
                     break
                 }
             }
+            
+//            Rectangle().fill(selectedColor)
+//            Picker("Favorite Color", selection: $selectedColor, content: {
+//                Text("Red").tag(Color.red)
+//                Text("Green").tag(Color.green)
+//                Text("Blue").tag(Color.blue)
+//            })
+//            .pickerStyle(SegmentedPickerStyle())
         }
     }
 }
