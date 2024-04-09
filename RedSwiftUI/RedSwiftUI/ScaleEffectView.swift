d71f453c54687a3f6c6a9ee15aac3bcd300ba625
uunwon <chea2089@naver.com>
2024-04-09 15:08:06 +0900

    Rotation Gesture

--- RedSwiftUI/RedSwiftUI/ScaleEffectView.swift
+++ RedSwiftUI/RedSwiftUI/ScaleEffectView.swift
@@ -13,6 +13,7 @@ struct ScaleEffectView: View {
     @State private var finalValue: CGFloat = 1
     // CGFloat: 그래픽 처리할 때 쓰는 Float
     // CGPoint: 그래픽 처리할 때 쓰는 좌표(x,y) 값
+    @State private var degree = 0.0 // 돌리기, 각도 이용
     
     var body: some View {
         VStack {
@@ -28,17 +29,26 @@ struct ScaleEffectView: View {
                 // .aspectRatio(contentMode: .fit) == scaledToFit
                 .scaledToFit() // 부모 크기에 고정해 맞춰줌
                 .frame(width: 200, height: 200)
-                .foregroundColor(.red)
-                .scaleEffect(finalValue + tempValue)
-                .gesture( // gesture modifier
-                    MagnificationGesture()
-                        // option amount 클릭하면 타입 나옴!!
-                        .onChanged{ amount in
-                            tempValue = amount - 1
-                        }
-                        .onEnded { amount in
-                            finalValue += tempValue
-                            tempValue = 0
+                .foregroundColor(.customWine)
+//                 .scaleEffect(finalValue + tempValue)
+//                .gesture(
+//                    MagnificationGesture()
+//                        // option amount 클릭하면 타입 나옴!!
+//                        .onChanged{ amount in
+//                            tempValue = amount - 1
+//                        }
+//                        .onEnded { amount in
+//                            finalValue += tempValue
+//                            tempValue = 0
+//                        }
+//                )
+            
+                // ✨ Rotation Gesture
+                .rotationEffect(Angle.degrees(degree)) // 각도로 앵글 만들기, 후 동작
+                .gesture( // gesture modifier, 선 동작
+                    RotationGesture()
+                        .onChanged{ angle in
+                            degree = angle.degrees
                         }
                 )
         }
