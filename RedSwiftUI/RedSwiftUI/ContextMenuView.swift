90b4364f341262908875725ce171e8643f3cdb70
uunwon <chea2089@naver.com>
2024-04-09 15:45:59 +0900

    Context Menu

---
+++ RedSwiftUI/RedSwiftUI/ContextMenuView.swift
@@ -0,0 +1,45 @@
+//
+//  ContextMenuView.swift
+//  RedSwiftUI
+//
+//  Created by uunwon on 4/9/24.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct ContextMenuView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "hare")
+                .padding(.top)
+            
+            Text("Context Menu")
+                .font(.custom("AmericanTypewriter", size: 24))
+                .bold()
+            
+            Image(systemName: "heart.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .foregroundColor(.customPink)
+                .padding()
+                .contextMenu { // 길게 누르면 동작
+                    Button {
+                        
+                    } label: {
+                        Label("Add to Favorite", systemImage: "heart")
+                    }
+                    Button {
+                        
+                    } label: {
+                        Label("Show in Maps", systemImage: "mappin")
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContextMenuView()
+}
