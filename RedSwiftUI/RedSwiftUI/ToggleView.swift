573ff277427cc533fc411e014ba818e10f5572e4
uunwon <chea2089@naver.com>
2024-04-09 13:13:02 +0900

    Toggle

--- RedSwiftUI/RedSwiftUI/ToggleView.swift
+++ RedSwiftUI/RedSwiftUI/ToggleView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ToggleView: View {
     @State var myToggle = true
     @State var newValue = 0
+    @State var sliderValue = 0.0
     
     var body: some View {
         VStack {
@@ -21,22 +22,32 @@ struct ToggleView: View {
                 .font(.custom("AmericanTypewriter", size: 24))
                 .bold()
             
+            // ✨ 토글 버튼 + 사각형 색상 변환
             Toggle(isOn: $myToggle) {
-                Text(myToggle ? "Orange" : "Green")
+                Text(myToggle ? "Orange" : "Mint")
             }
             .padding()
-            .foregroundColor(myToggle ? .orange : .green)
-            .tint(myToggle ? .orange : .green)
+            .foregroundColor(myToggle ? .orange : .mint)
+            .tint(myToggle ? .orange : .mint)
             
             Rectangle()
                 .frame(width: 500, height: 100)
-                .foregroundColor(myToggle ? .orange : .green)
+                .foregroundColor(myToggle ? .orange : .mint)
             
+            // ✨ 스테퍼: 사용자가 값을 증가시키거나 감소하며 세분화된 컨트롤
             Stepper(value: $newValue, in: 1...10) {
                 Text("Stepper Value = \(newValue)")
             }
             .padding()
-            .foregroundColor(myToggle ? .orange : .green)
+            .foregroundColor(myToggle ? .orange : .mint)
+            
+            // ✨ 슬라이더: 연속 값 범위 내 단일 값 선택하는 컨트롤
+            Slider(value: $sliderValue, in: 1...50, step: 4)
+                .padding()
+                .tint(myToggle ? .orange : .mint)
+            
+            Text("Slider value = \(sliderValue)")
+                .foregroundColor(myToggle ? .orange : .mint)
         }
     }
 }
