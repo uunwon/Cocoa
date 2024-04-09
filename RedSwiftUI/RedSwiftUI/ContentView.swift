5ba9efc42408a4941c7f16b9ecad6dfe9a13ea1b
uunwon <chea2089@naver.com>
2024-04-09 11:37:02 +0900

    Date formatter

--- RedSwiftUI/RedSwiftUI/ContentView.swift
+++ RedSwiftUI/RedSwiftUI/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("൹")
+            Image(systemName: "hare")
                 .padding(.top)
             
             Text("Title")
--- RedSwiftUI/RedSwiftUI/DatePickerView.swift
+++ RedSwiftUI/RedSwiftUI/DatePickerView.swift
@@ -14,10 +14,12 @@ struct DatePickerView: View {
         let calendar = Calendar.current
         let startComponents = DateComponents(year: 2024, month: 3, day: 25)
         let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)!
+        return calendar.date(from: startComponents)! // 언래핑
         ... // 범위 지정 1...10 처럼
         calendar.date(from:endComponents)!
-    }()
+    }() // 괄호를 통해 선언부가 바로 실행이 가능해짐 - 마치 함수처럼
+    
+    let formatter = DateFormatter() // 날짜 정보를 문자열로 출력하기 위한 객체
     
     var body: some View {
         VStack {
@@ -28,13 +30,20 @@ struct DatePickerView: View {
                 .font(.custom("AmericanTypewriter", size: 24))
                 .bold()
             
-            Text("Chosen Date = \(myDate)")
+            Text("Chosen Date = \(formatter.string(from:myDate))")
+                .underline()
                 .padding()
             
             DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: { Text("Date") })
                 .datePickerStyle(.graphical)
                 .padding()
                 .tint(.black)
+            
+        }.onAppear() {
+            formatter.locale = Locale(identifier: "ko_KR")
+            // formatter.dateStyle = .full
+            formatter.dateStyle = .long
+            formatter.timeStyle = .medium
         }
     }
 }
