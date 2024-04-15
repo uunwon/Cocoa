6936b1640d3bbe8eca7583bd2c60090f2987d253
uunwon <chea2089@naver.com>
2024-04-15 17:05:05 +0900

    start OX Quiz app

---
+++ OXQuiz/OXQuiz/OXQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXQuizApp.swift
+//  OXQuiz
+//
+//  Created by uunwon on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct OXQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
