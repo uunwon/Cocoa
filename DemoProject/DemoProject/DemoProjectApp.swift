5079d0b0ebe226000a5ab4be08578d5889b01627
uunwon <chea2089@naver.com>
2024-04-17 13:15:27 +0900

    start Demo Project

---
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by uunwon on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
