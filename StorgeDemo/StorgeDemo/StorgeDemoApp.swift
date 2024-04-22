ffcc594a07da2fbfd871ffcf8da2ebd38b91274e
uunwon <chea2089@naver.com>
2024-04-22 09:10:47 +0900

    Initialize Project StorageDemo

---
+++ StorgeDemo/StorgeDemo/StorgeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorgeDemoApp.swift
+//  StorgeDemo
+//
+//  Created by uunwon on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct StorgeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
