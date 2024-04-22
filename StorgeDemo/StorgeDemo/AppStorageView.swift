c4c9bafc8b46956734be4211d06c7b92cfd05cc4
uunwon <chea2089@naver.com>
2024-04-22 10:09:55 +0900

    add View

---
+++ StorgeDemo/StorgeDemo/AppStorageView.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorageView.swift
+//  StorgeDemo
+//
+//  Created by uunwon on 4/22/24.
+//
+
+import SwiftUI
+
+struct AppStorageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorageView()
+}
---
+++ StorgeDemo/StorgeDemo/SceneStorageView.swift
@@ -0,0 +1,22 @@
+//
+//  SceneStorageView.swift
+//  StorgeDemo
+//
+//  Created by uunwon on 4/22/24.
+//
+
+import SwiftUI
+
+struct SceneStorageView: View {
+    @SceneStorage("mytext") private var editorText: String = ""
+    
+    var body: some View {
+        TextEditor(text: $editorText)
+            .padding(38)
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    SceneStorageView()
+}
