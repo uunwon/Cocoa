aa0524dcf9d913767dda29033ddcc7c4553e8e12
uunwon <chea2089@naver.com>
2024-04-25 14:18:26 +0900

    start ToDoList

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by uunwon on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
