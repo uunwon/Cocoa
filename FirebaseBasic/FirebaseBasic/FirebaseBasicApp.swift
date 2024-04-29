ab1d092b0c99b398c42e9e3206734784f805664a
uunwon <chea2089@naver.com>
2024-04-29 13:38:01 +0900

    Initialize Project FirebaseBasic

---
+++ FirebaseBasic/FirebaseBasic/FirebaseBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBasicApp.swift
+//  FirebaseBasic
+//
+//  Created by uunwon on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
