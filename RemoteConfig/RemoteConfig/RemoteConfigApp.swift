68ba46cc98767af48b700790281b285a4e38e65d
uunwon <chea2089@naver.com>
2024-04-29 11:03:36 +0900

    Initialize Project RemoteConfig

---
+++ RemoteConfig/RemoteConfig/RemoteConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigApp.swift
+//  RemoteConfig
+//
+//  Created by uunwon on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
