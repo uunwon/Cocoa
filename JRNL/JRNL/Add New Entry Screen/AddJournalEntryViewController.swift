c1ff5ee8447531023488b42cdf379f21c12c284d
uunwon <chea2089@naver.com>
2024-05-10 14:15:16 +0900

    update new version

--- JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class AddJournalEntryViewController: UIViewController {
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var bodyTextView: UITextView!
+    @IBOutlet var photoImageView: UIImageView!
+    
+    var newJournalEntry: JournalEntry?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +22,19 @@ class AddJournalEntryViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let title = titleTextField.text ?? ""
+        let body = bodyTextView.text ?? ""
+        let photo = photoImageView.image
+        let rating = 3
+        newJournalEntry = JournalEntry(rating: rating, title: title,
+                                       body: body, photo: photo)
     }
-    */
 
 }
--- JRNL/JRNL/Journal List Screen/JournalListViewController.swift
+++ JRNL/JRNL/Journal List Screen/JournalListViewController.swift
@@ -37,5 +37,14 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
     @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
         
     }
+    @IBAction func unwindNewEntrySave(segue: UIStoryboardSegue) {
+        if let sourceViewController = segue.source as?
+            AddJournalEntryViewController, let newJournalEntry = sourceViewController.newJournalEntry {
+            sampleJournalEntryData.journalEntries.append(newJournalEntry)
+            tableView.reloadData()
+        }
+    }
+    
+    
 }
 
