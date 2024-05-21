dfe18527bc614315e31a2ba7a92b6dfb4c182312
uunwon <chea2089@naver.com>
2024-05-21 16:15:36 +0900

    ViewController 파일 비우기

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -1,74 +1,16 @@
 //
-//  WebViewController.swift
+//  ViewController.swift
 //  AutoLayout
 //
-//  Created by uunwon on 5/21/24.
+//  Created by uunwon on 5/20/24.
 //
 
 import UIKit
-import WebKit
-import SafariServices
 
 class ViewController: UIViewController {
-    private lazy var webView: WKWebView = {
-        let webView = WKWebView(frame: .zero)
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        webView.isHidden = true
-        return webView
-    }()
-    
-    private lazy var closeButton: UIButton = {
-        let button = UIButton(type: .close)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addAction(UIAction { [weak self] _ in
-            print("button cliked")
-            self?.webView.isHidden = true
-        }, for: .touchUpInside)
-        return button
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let linkButton = UIButton(type: .system)
-        var conf = UIButton.Configuration.filled()
-        conf.title = "Apple"
-        linkButton.configuration = conf
-        linkButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-        
-        linkButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        linkButton.addAction(UIAction { [weak self] _ in
-            if let url = URL(string:"https://www.apple.com") {
-                 self?.openInWebView(url: url)
-                
-//                let safariViewController = SFSafariViewController(url: url) // 다양한 형태의 모달 뷰로 띄울 수 있음
-//                safariViewController.modalPresentationStyle = .pageSheet
-//                self?.present(safariViewController, animated: true)
-            }
-        }, for: .touchUpInside)
-        
-        view.addSubview(linkButton)
-        webView.addSubview(closeButton)
-        view.addSubview(webView)
-
-        NSLayoutConstraint.activate([
-            linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
-            closeButton.topAnchor.constraint(equalTo: webView.topAnchor, constant: -30),
-            closeButton.trailingAnchor.constraint(equalTo: webView.trailingAnchor, constant: -10)
-        ])
-    }
-
-    func openInWebView(url: URL) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-        webView.isHidden = false
     }
 }
