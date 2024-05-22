3f9d3bebe6d24f2ea65a57b3ed6a46bb60238cf1
uunwon <chea2089@naver.com>
2024-05-22 09:37:45 +0900

    LongPress

--- AutoLayout/AutoLayout/0522/GestureViewController.swift
+++ AutoLayout/AutoLayout/0522/GestureViewController.swift
@@ -14,19 +14,36 @@ class GestureViewController: UIViewController {
 
         let rectangle = UIView()
         rectangle.backgroundColor = .banana
-        rectangle.frame = CGRect(x: 0 , y: 0, width: 1000, height: 1000) // AutoLayout 안 쓰고 옛날 frame 방식으로-
+        rectangle.frame = CGRect(x: 100 , y: 100, width: 175, height: 125) // AutoLayout 안 쓰고 옛날 frame 방식으로-
         rectangle.isUserInteractionEnabled = true // 터치를 받을 수 있다 라는 속성
         
-        view.addSubview(rectangle)
+        let rectangle2 = UIView()
+        rectangle2.backgroundColor = .banana
+        rectangle2.frame = CGRect(x: 100 , y: 300, width: 175, height: 125)
+        rectangle2.isUserInteractionEnabled = true
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addSubview(rectangle)
+        view.addSubview(rectangle2)
+        
+        /* TAP */
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
         tapGesture.numberOfTapsRequired = 2 // 두번 누르면 tap, default 는 1번
         rectangle.addGestureRecognizer(tapGesture) // 뷰에 제스처를 붙힘
         
+        /* Long Press */
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(longGesture))
+        longGesture.minimumPressDuration = 2.0 // 길게 눌리는 시간
+        rectangle2.addGestureRecognizer(longGesture)
     }
     
-    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+    @objc func tapGesture(_ sender: UITapGestureRecognizer) {
         if let view = sender.view {
+            view.backgroundColor = (view.backgroundColor == .banana) ? .lightSalmon : .banana
+        }
+    }
+    
+    @objc func longGesture(_ sender: UITapGestureRecognizer) {
+        if let view = sender.view, sender.state == .began {
             view.backgroundColor = (view.backgroundColor == .banana) ? .lightSalmon : .banana
         }
     }
