353a1cac79d81c31f185b2030894317176500b65
uunwon <chea2089@naver.com>
2024-05-22 10:38:22 +0900

    Rotation

--- AutoLayout/AutoLayout/0522/GestureViewController.swift
+++ AutoLayout/AutoLayout/0522/GestureViewController.swift
@@ -23,12 +23,19 @@ class GestureViewController: UIViewController {
         rectangle2.isUserInteractionEnabled = true
         
         let imageView = UIImageView(image: UIImage(systemName: "water.waves"))
-        imageView.frame = CGRect(x:100, y: 400, width: 175, height: 125)
+        imageView.frame = CGRect(x:100, y: 410, width: 175, height: 125)
         imageView.isUserInteractionEnabled = true
+        
+        let imageView2 = UIImageView(image: UIImage(systemName: "fleuron"))
+        // bounds 는 실측 사이즈 (화면에서 나타나는 나의 크기)
+        imageView2.frame = CGRect(x: view.bounds.midX - 70, y: view.bounds.midY + 140, width: 125, height: 125)
+        imageView2.tintColor = .lightMint
+        imageView2.isUserInteractionEnabled = true
         
         view.addSubview(rectangle)
         view.addSubview(rectangle2)
         view.addSubview(imageView)
+        view.addSubview(imageView2)
         
         /* TAP */
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
@@ -43,6 +50,10 @@ class GestureViewController: UIViewController {
         /* Pinch */
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinchGesture))
         imageView.addGestureRecognizer(pinchGesture)
+        
+        /* Rotation */
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(rotationGesture))
+        imageView2.addGestureRecognizer(rotationGesture)
     }
     
     @objc func tapGesture(_ sender: UITapGestureRecognizer) {
@@ -76,6 +87,13 @@ class GestureViewController: UIViewController {
             if currentScale < 1.2 { // 최댓값
                 view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
             }
+        }
+    }
+    
+    @objc func rotationGesture(_ sender: UIRotationGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.rotated(by: sender.rotation)
+            sender.rotation = 0
         }
     }
     
