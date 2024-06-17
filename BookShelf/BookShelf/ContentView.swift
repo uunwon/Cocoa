be28a2ae298b87f7ef7bab0142031f16b2795fcb
uunwon <chea2089@naver.com>
2024-06-17 10:30:08 +0900

    Add Assets & 책 소개 한줄 UI

--- BookShelf/BookShelf/ContentView.swift
+++ BookShelf/BookShelf/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(alignment: .top) {
+            Image("9781484285718-M")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 90)
+            VStack(alignment: .leading) {
+                Text("Asynchronous Programming with SwiftUI and Combine")
+                    .font(.headline)
+                Text("by Peter Friess")
+                    .font(.subheadline)
+                Text("451 pages")
+                    .font(.subheadline)
+            }
         }
-        .padding()
     }
 }
 
