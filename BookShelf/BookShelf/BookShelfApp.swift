4a13d60b0f2c91b31d40e24878ea38c1783aebf5
uunwon <chea2089@naver.com>
2024-06-17 10:16:34 +0900

    Return SwiftUI

---
+++ BookShelf/BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by uunwon on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
